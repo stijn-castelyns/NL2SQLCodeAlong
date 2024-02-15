b7b9f9f3bb86e5fafe273405071fe6cc07892c24
stijn-castelyns <stijn@u2u.be>
2024-02-15 12:10:31 +0100

    Removed EF Core functionality

--- NL2SQL.Terminal/Program.cs
+++ NL2SQL.Terminal/Program.cs
@@ -1,13 +1,15 @@
 ﻿using Azure;
 using Azure.AI.OpenAI;
 using Azure.Core;
-using Microsoft.EntityFrameworkCore;
+using Dapper;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
-using NL2SQL.Infra;
+using System.Data;
 using System.Reflection;
 using System.Text.Json;
+using System.Xml;
 
 (AzureKeyCredential? azureKeyCredential, string? deploymentName, Uri? endpoint) = LoadConfiguration();
 OpenAIClient openAIClient = new(endpoint, azureKeyCredential);
@@ -25,21 +27,35 @@ KernelFunction nl2TsqlTranslator = kernel.CreateFunctionFromPromptYaml(
     promptTemplateFactory: new HandlebarsPromptTemplateFactory()
 );
 
-//Console.ForegroundColor = ConsoleColor.Green;
-//Console.WriteLine("Assistant > Hello! I am an assistant that let's you use natural language to query the Northwind Sql Server database. How can I be of use to you?");
-//Console.ForegroundColor = ConsoleColor.Cyan;
-//Console.Write("User > ");
-//var request = Console.ReadLine()!;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Assistant > Hello! I am an assistant that let's you use natural language to query the Northwind Sql Server database. How can I be of use to you?");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.Write("User > ");
+var request = Console.ReadLine()!;
 
-//Console.ForegroundColor = ConsoleColor.Yellow;
-//Console.WriteLine("++++++++ The Generated T-SQL Query ++++++++\n");
-//var result = kernel.InvokeAsync(nl2TsqlTranslator, new() {
-//            { "request", request } });
-//Console.WriteLine(result.Result);
-//Console.WriteLine("\n+++++++++++++++++++++++++++++++++++++++++++");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("++++++++ The Generated T-SQL Query ++++++++\n");
+var result = kernel.InvokeAsync(nl2TsqlTranslator, new() {
+            { "request", request } });
+string generatedQuery = result.Result.ToString();
+Console.WriteLine(generatedQuery);
+Console.WriteLine("\n+++++++++++++++++++++++++++++++++++++++++++");
 
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("+++++++++ The Results of the Query ++++++++\n");
 
-
+using (IDbConnection db = new SqlConnection("Data Source=(localdb)\\tsql;Initial Catalog=Northwind;Integrated Security=True;Encrypt=True"))
+{
+    var queryResult = db.Query(generatedQuery);
+    var options = new JsonSerializerOptions
+    {
+        WriteIndented = true
+    };
+    var json = JsonSerializer.Serialize(queryResult,options);
+    Console.WriteLine(json);
+}
+Console.WriteLine("\n+++++++++++++++++++++++++++++++++++++++++++");
+Console.ForegroundColor = ConsoleColor.White;
 (AzureKeyCredential? azureKeyCredential, string? deploymentName, Uri? endpoint) LoadConfiguration()
 {
     IConfigurationRoot config = new ConfigurationBuilder()
